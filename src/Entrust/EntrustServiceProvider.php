30af941c378ecd289f614067881e6645c5e221fc
Carlos Villarroel <cevv13@hotmail.com>
2020-10-07 17:47:21 -0400

    actualizar ServiceProvider

--- src/Entrust/EntrustServiceProvider.php
+++ src/Entrust/EntrustServiceProvider.php
@@ -8,6 +8,7 @@
  * @package Cevv13\Entrust
  */
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class EntrustServiceProvider extends ServiceProvider
@@ -33,7 +34,7 @@ class EntrustServiceProvider extends ServiceProvider
 
         // Register commands
         $this->commands('command.entrust.migration');
-        
+
         // Register blade directives
         $this->bladeDirectives();
     }
@@ -59,30 +60,32 @@ class EntrustServiceProvider extends ServiceProvider
      */
     private function bladeDirectives()
     {
+        if (!class_exists('\Blade')) return;
+
         // Call to Entrust::hasRole
-        \Blade::directive('role', function($expression) {
+        Blade::directive('role', function($expression) {
             return "<?php if (\\Entrust::hasRole({$expression})) : ?>";
         });
 
-        \Blade::directive('endrole', function($expression) {
+        Blade::directive('endrole', function($expression) {
             return "<?php endif; // Entrust::hasRole ?>";
         });
 
         // Call to Entrust::can
-        \Blade::directive('permission', function($expression) {
+        Blade::directive('permission', function($expression) {
             return "<?php if (\\Entrust::can({$expression})) : ?>";
         });
 
-        \Blade::directive('endpermission', function($expression) {
+        Blade::directive('endpermission', function($expression) {
             return "<?php endif; // Entrust::can ?>";
         });
 
         // Call to Entrust::ability
-        \Blade::directive('ability', function($expression) {
+        Blade::directive('ability', function($expression) {
             return "<?php if (\\Entrust::ability({$expression})) : ?>";
         });
 
-        \Blade::directive('endability', function($expression) {
+        Blade::directive('endability', function($expression) {
             return "<?php endif; // Entrust::ability ?>";
         });
     }
@@ -97,7 +100,7 @@ class EntrustServiceProvider extends ServiceProvider
         $this->app->bind('entrust', function ($app) {
             return new Entrust($app);
         });
-        
+
         $this->app->alias('entrust', 'Cevv13\Entrust\Entrust');
     }
 
